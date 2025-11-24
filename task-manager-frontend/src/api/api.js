d1d4f1b4cab39361e125617e9063244d0cf49bdc
Raghuveer Diya <143068995+raghuveer2410@users.noreply.github.com>
2025-11-24 15:23:48 +0530

    Update api.js

--- task-manager-frontend/src/api/api.js
+++ task-manager-frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://task-manager-jzey.onrender.com/api",
+  baseURL: "https://task-manager-jzey.onrender.com/api/",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
